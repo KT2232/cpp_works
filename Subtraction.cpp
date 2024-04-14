72ffadbe9268bfe58b35df65c8f3dfdc4a23ee0e
Karthik Tarodekar <140381778+KT2232@users.noreply.github.com>
2024-04-14 16:18:29 +0530

    Create Subtraction.cpp

---
+++ Subtraction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int a,b,diff;
+
+  cout<<"Enter number 1:";
+  cin>>a;
+
+  cout<<"Enter number 2:";
+  cin>>b;
+
+  diff = a - b;
+
+  cout<<"Subtraction of "<<a<<" and "<<b<<" comes out as "<<diff;
+
+return 0;
+}
