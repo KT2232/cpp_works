8857c6ccbe2fd4f454ec5daab92fdace156ef6ca
Karthik Tarodekar <140381778+KT2232@users.noreply.github.com>
2024-04-14 16:40:16 +0530

    Create Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std
+
+int main () 
+{
+
+  int a,b,sum;
+
+  cout<<"Enter number 1:";
+  cin>>a;
+
+  cout<<"Enter number 2:";
+  cin>>b;
+
+  sum = a+b;
+
+  cout<<"Sum of "<<a<<" and "<<b<<" comes out as "<<sum;
+
+  return 0;
+}
